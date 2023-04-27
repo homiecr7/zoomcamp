ea975d53a20ae0bf5452dc28d59d5aac5529a7ee
homiecr7 <hammadazhar1997@gmail.com>
2023-04-27 11:37:00 +0000

    setup prefect and added flows and connections

--- Week_2/3_prefect/injest.py
+++ Week_2/3_prefect/injest.py
@@ -10,6 +10,7 @@ from time import time
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
 
 @task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def extract_data(url):
@@ -26,23 +27,25 @@ def extract_data(url):
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.drop("Unnamed: 0", axis="columns", inplace=True)
 
-    return df, df_iter
+    return df
 
 @task(log_prints=True)
 def transform_data(df):
     print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
-    df = df["passenger_count" != 0]
+    df = df[df["passenger_count"] != 0]
     print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
     return(df)
 
 @task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, df):
-    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
-    df.head(0).to_sql(name=table_name, con=engine, if_exists="replace")
-    print("Header Inserted")
-    df.to_sql(name=table_name, if_exists="append", con=engine)
-    # print("First Itration Inserted")
-    print("Itration Inserted")
+def ingest_data(table_name, df):
+    connection_block = SqlAlchemyConnector.load("postgres-block")
+
+    with connection_block.get_connection(begin=False) as engine:
+        df.head(0).to_sql(name=table_name, con=engine, if_exists="replace")
+        print("Header Inserted")
+        df.to_sql(name=table_name, if_exists="append", con=engine)
+        # print("First Itration Inserted")
+        print("Itration Inserted")
 
     # try:
     #     while True:
@@ -60,19 +63,20 @@ def ingest_data(user, password, host, port, db, table_name, df):
     # except StopIteration as e:
     #     print(f"All itrations inserted so {e} in place")
 
+@flow(name="Subflow", log_prints=True)
+def log_subflow(table_name:str):
+    print("Logging Subflow for: {table_name}")
+
 @flow(name = "Ingest Flow")
 def main_flow():
-    user = "root"
-    password = "root"
-    host = "localhost"
-    port = "5431"
-    db = "ny_taxi"
     table_name = "yellow_taxi_trips"
     url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet"
 
-    raw_data, itr_object = extract_data(url)
+    log_subflow(table_name)
+    raw_data = extract_data(url)
+    print("transforming data")
     transformed_data = transform_data(raw_data)
-    ingest_data(user, password, host, port, db, table_name, transformed_data)
+    ingest_data(table_name, transformed_data)
 
 if __name__=='__main__':
     main_flow()
